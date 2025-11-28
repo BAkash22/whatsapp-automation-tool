2a32bb13274ea40d1cb847c1853febfd41d42427
Akash B <95532025+BAkash22@users.noreply.github.com>
2025-11-28 22:30:11 +0530

    Update app.py

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,1 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return jsonify({"message": "WhatsApp Automation Backend Running!"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
